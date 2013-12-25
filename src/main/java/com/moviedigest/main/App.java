1ab1ded05d7dc7052f2ca0038f8fd5c877ce5922
Makarand Salvi <makarand.salvi@neptune-int.com>
2013-12-25 12:45:37 +0530

    Improved Search -- (subDirectory included)

--- src/main/java/com/moviedigest/main/App.java
+++ src/main/java/com/moviedigest/main/App.java
@@ -6,7 +6,8 @@ public class App
     public static void main(String[] args)
     {
         Main main = new Main();
-        main.MovieFunctions();main.SearchFunctions();
+        main.MovieFunctions();
+        main.SearchFunctions();
     }
 
 }
--- src/main/java/com/moviedigest/service/impl/SearchDirectoriesServiceImpl.java
+++ src/main/java/com/moviedigest/service/impl/SearchDirectoriesServiceImpl.java
@@ -1,6 +1,7 @@
 package com.moviedigest.service.impl;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,37 @@ import com.moviedigest.service.SearchDirectoriesService;
 
 public class SearchDirectoriesServiceImpl implements SearchDirectoriesService
 {
+    private List<String> fileNames = new ArrayList<String>();
 
     public List<String> getFilenames(String directoryPath)
     {
-        File directory = new File(directoryPath);
-        return Arrays.asList(directory.list());
+        File originalDirectory = new File(directoryPath);
+        obtainFileNames(originalDirectory);
+        return fileNames;
+    }
+
+    private List<String> obtainFileNames(File originalDirectory)
+    {
+        List<File> filesAndFolders = Arrays.asList(originalDirectory.listFiles());
+        for (File file : filesAndFolders)
+        {
+            if (fileIsDirectory(file))
+            {
+                obtainFileNames(file);
+            }
+            else
+            {
+                fileNames.add(file.getName());
+            }
+        }
+        return fileNames;
+    }
+
+    private boolean fileIsDirectory(File file)
+    {
+        if (file.isDirectory())
+            return true;
+        return false;
     }
 
 }
